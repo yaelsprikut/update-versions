50c44617b5a2dbc01ae7291c3ce8c60ca57088a0
Yael Sprikut <yael@YaelMacPro.local>
2021-03-16 18:46:34 -0400

    first commit :)

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Yael's Wonderland. Nikita Silvestrov is the best....</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+  );
+}
+
+export default App;
