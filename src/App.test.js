ae0d81f5331f9533e2107f4c9e8a2ddc5f941a4c
Yael Sprikut <yael@YaelMacPro.local>
2021-03-16 23:44:50 -0400

    prettier

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Update your package link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/classical music plays/i);
   expect(linkElement).toBeInTheDocument();
 });
